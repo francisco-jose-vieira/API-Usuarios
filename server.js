c5c3ce2e3d34267c14520dade03904bcb684568e
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-02-03 22:09:43 -0300

    Pequenas alterações

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors"
 const app = express();
+
 app.use(express.json());
+app.use(cors())
 
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
